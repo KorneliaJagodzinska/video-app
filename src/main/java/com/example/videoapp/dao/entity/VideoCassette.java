6340bbf7987c63c64d3b0aa3143827bc4305101c
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-10-18 14:55:24 +0200

    entity

--- src/main/java/com/example/videoapp/dao/entity/VideoCassette.java
+++ src/main/java/com/example/videoapp/dao/entity/VideoCassette.java
@@ -1,14 +1,21 @@
-package com.example.videoapp;
+package com.example.videoapp.dao.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class VideoCassette {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
     private String title;
     private LocalDate productionYear;
